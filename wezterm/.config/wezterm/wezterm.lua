68a397f445b60500a446fc79aa2ffd350c963f0c
jihun-yoon <jihun.yoon@doosan.com>
2026-09-17 16:35:29 +0900

    feat: add keyboard window switching

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -94,6 +94,16 @@ config.status_update_interval = 10000
 -- Move the active WezTerm pane into its own window without using a shell.
 config.keys = {
   {
+    key = "`",
+    mods = "CMD",
+    action = wezterm.action.ActivateWindowRelative(1),
+  },
+  {
+    key = "`",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.ActivateWindowRelative(-1),
+  },
+  {
     key = "UpArrow",
     mods = "CMD",
     action = wezterm.action.ScrollByPage(-1),
